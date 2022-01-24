3278d10639b20b63556e8bef7e86f5a0d72bc1f7
Reykira007 <rearryc@gmail.com>
2022-01-25 06:24:21 +0700

    historyMenu

--- app/src/main/java/ac/id/atmaluhur/mhs/tourguide/History.java
+++ app/src/main/java/ac/id/atmaluhur/mhs/tourguide/History.java
@@ -2,23 +2,22 @@ package ac.id.atmaluhur.mhs.tourguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Pair;
 import android.view.View;
 
-public class Makanan extends AppCompatActivity {
+public class History extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_makanan);
+        setContentView(R.layout.activity_history);
     }
 
-    public void callDashboard(View view){
+    public void callWisata(View view){
 
-        startActivity(new Intent(getApplicationContext(),Dashboard.class));
+        startActivity(new Intent(getApplicationContext(),Wisata.class));
 
     }
+
 }
